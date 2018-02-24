2333420054afa5cb3d5f7b2bcf62d3d1060581c8
Julio Gutierrez <bubuntux@gmail.com>
2018-02-24 12:26:45 -0700

    adapt container to connect by default to the recomended NordVpn server

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
-FROM alpine
-MAINTAINER David Personette <dperson@gmail.com>
+FROM alpine:3.7
 
-# Install openvpn
+LABEL maintainer="Julio Gutierrez <bubuntux@gmail.com>"
+
+COPY nordVpn.sh /usr/bin
+CMD nordVpn.sh
+
+HEALTHCHECK --start-period=5s --timeout=15s --interval=60s \
+			CMD curl -fL 'https://api.ipify.org' || exit 1
+
+	# Install dependencies 
 RUN apk --no-cache --no-progress upgrade && \
-    apk --no-cache --no-progress add bash curl ip6tables iptables openvpn \
-                shadow && \
-    addgroup -S vpn && \
-    rm -rf /tmp/*
-
-COPY openvpn.sh /usr/bin/
-
-HEALTHCHECK --interval=60s --timeout=15s --start-period=120s \
-             CMD curl -L 'https://api.ipify.org'
-
-VOLUME ["/vpn"]
-
-ENTRYPOINT ["openvpn.sh"]
+    apk --no-cache --no-progress add curl unzip iptables ip6tables jq openvpn && \
+	# Download ovpn files
+	curl https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip -o /tmp/ovpn.zip && \
+    unzip -q /tmp/ovpn.zip -d /tmp/ovpn && \
+    mkdir -p /vpn/ovpn/ && \
+    mv /tmp/ovpn/*/*.ovpn /vpn/ovpn/ && \
+	# Cleanup
+	rm -rf /tmp/*
---
+++ nordVpn.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# Firewall everything has to go through the vpn
+iptables  -F OUTPUT
+ip6tables -F OUTPUT 2> /dev/null
+iptables  -P OUTPUT DROP
+ip6tables -P OUTPUT DROP 2> /dev/null
+iptables  -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
+ip6tables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -o lo -j ACCEPT
+ip6tables -A OUTPUT -o lo -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -o tun0 -j ACCEPT 
+ip6tables -A OUTPUT -o tun0 -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -d `ip -o addr show dev eth0 | awk '$3 == "inet" {print $4}'` -j ACCEPT
+ip6tables -A OUTPUT -d `ip -o addr show dev eth0 | awk '$3 == "inet6" {print $4; exit}'` -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -p udp --dport 53 -j ACCEPT
+ip6tables -A OUTPUT -p udp --dport 53 -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -o eth0 -p udp --dport 1194 -j ACCEPT 
+ip6tables -A OUTPUT -o eth0 -p udp --dport 1194 -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -o eth0 -p tcp --dport 1194 -j ACCEPT 
+ip6tables -A OUTPUT -o eth0 -p tcp --dport 1194 -j ACCEPT 2> /dev/null
+iptables  -A OUTPUT -o eth0 -d nordvpn.com -j ACCEPT
+ip6tables -A OUTPUT -o eth0 -d nordvpn.com -j ACCEPT 2> /dev/null
+
+base_dir="/vpn"
+ovpn_dir="$base_dir/ovpn"
+auth_file="$base_dir/auth"
+
+# Get NordVpn server recomendations
+recomendations=`curl -s https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations |\
+                jq -r '.[] | .hostname' | shuf`
+
+for recomendation in ${recomendations}; do # Prefer UDP
+    config_file="${ovpn_dir}/${recomendation}.udp.ovpn"
+    if [ -r "$config_file" ]; then
+        config="$config_file"
+        break
+    fi
+done
+if [ -z $config ]; then # Use TCP if UDP not available
+   for recomendation in ${recomendations}; do
+        config_file="${ovpn_dir}/${recomendation}.tcp.ovpn"
+        if [ -r "$config_file" ]; then
+            config="$config_file"
+            break
+        fi
+    done
+fi
+if [ -z $config ]; then # If recomendation was not found, use a random server
+    config="${ovpn_dir}/`ls ${ovpn_dir} | shuf -n 1`"
+fi
+
+# Create auth_file
+echo "$USER" > $auth_file 
+echo "$PASS" >> $auth_file
+chmod 0600 $auth_file
+
+openvpn --cd $base_dir --config $config \
+    --auth-user-pass $auth_file --auth-nocache \
+    --script-security 2 --up /etc/openvpn/up.sh --down /etc/openvpn/down.sh
